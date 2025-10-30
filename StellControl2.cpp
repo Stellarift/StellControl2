468356bf861c47b62f6faff31f2080a0afc3232d
Stellarift <filippov2888@gmail.com>
2025-10-30 19:27:28 +0900

    Add files via upload

---
+++ StellControl2.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+
+//Задание 1(типы данных и ввод-вывод данных) №1
+/*int main() {
+	char symbol = 'A';
+	cout << symbol << endl;
+	return 0;
+}*/
+
+//Задание 2(операторы и условные конструкции) №4
+/*int main() {
+	setlocale(LC_ALL, "ru");
+	int number;
+	cout << "Введите число: ";
+	cin >> number;
+
+	if (number % 2 == 0) {
+		cout << "Число чётное" << endl;
+	}
+	else {
+		cout << "Число нечётное" << endl;
+	}
+
+	return 0;
+}*/
+
+//Задание 3(циклы и последовательности) №7
+/*int main() {
+	for (int i = 1; i <= 10; i++) {
+		cout << i * i * i << " ";
+	}
+	cout << endl;
+	return 0;
+}*/
+
+//Задание 4(одномерные массивы) №11
+/*int main() {
+	setlocale(LC_ALL, "ru");
+	const int arr[8] = { 45, 12, 78, 23, 89, 56, 34, 67 };
+	int max = arr[0];
+
+	for (int i = 1; i < 8; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	cout << "Максимальный эленмент: " << max << endl;
+	return 0;
+}*/
+
+//Задание 5(двухмерные массивы и вложенные циклы) №13
+/*int main() {
+	int n = 1;
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << n++ << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}*/
